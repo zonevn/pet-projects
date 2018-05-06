87973521faee4cbcb7905847b7ff1d46b4441dbe
zonevn <datazone.vn@gmail.com>
2018-05-07 06:57:34 +0700

    at first

---
+++ Arduino/build/project/src/app/models/ArduinoModel.java
@@ -0,0 +1,24 @@
+package app.models;
+
+import utils.SerialComm;
+
+public class ArduinoModel {
+	private SerialComm serialComm;
+
+	public ArduinoModel() {
+		
+	}
+
+	public void connect() {
+
+	}
+
+	public void disconnect() {
+
+	}
+
+	public void sedReq(String req) {
+
+	}
+
+}
